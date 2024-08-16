c4b273df2f993ccd7f656bb970599f5cec2117bb
Skye <poslert@oregonstate.edu>
2024-08-15 22:35:32 -0700

    Implemented base navigation

---
+++ react-resume/src/pages/homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from './Nav';
+
+const HomePage = () => {
+    return (
+    <div>
+        <header>
+            <h1>Homepage</h1>
+            <main>
+                <p>Testing homepage here!!</p>
+            </main>
+        </header>
+    </div>
+    );
+}
+
+export default HomePage;
